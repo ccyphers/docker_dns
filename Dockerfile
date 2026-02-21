2901f3c75b2b30648f7ab5e1ec6be9032f5fdc4b
Cliff Cyphers <cliff.cyphers@gmail.com>
2026-02-21 11:44:48 -0600

    removing combinded privoxy docker and pihole image

--- Dockerfile
+++ Dockerfile
@@ -3,21 +3,11 @@ FROM debian:bookworm-slim
 
 ADD init.sh /init.sh
 
-ADD setupVars.conf /etc/pihole/setupVars.conf
-
-ADD basic-install.sh /basic-install.sh
 
 RUN apt update && \
-    apt install -y vim dnss curl net-tools privoxy && \
-    chmod 755 /init.sh /basic-install.sh && \
-    cat /basic-install.sh | PIHOLE_SKIP_OS_CHECK=true bash /dev/stdin --unattended
+    apt install -y dnss ca-certificates    && \
+    chmod 755 /init.sh
 
-COPY etc-pihole /etc/pihole
-
-COPY dnsmasq.conf /etc/dnsmasq.conf
-
-COPY etc-dnsmasq.d /etc/dnsmasq.d
-    
 EXPOSE 53/udp
 WORKDIR /
 CMD ["/init.sh"]
--- init.sh
+++ init.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
-ifconfig lo:0 127.0.0.2
-
-nohup dnss -enable_dns_to_https -https_upstream="https://9.9.9.11/dns-query" -enable_dns_to_https  -dns_listen_addr=127.0.0.2:53 &
-
-#cd /usr/bin
-#setcap = pihole-FTL
-#./pihole-FTL
-ip=`ifconfig eth0 | grep inet | awk -F"inet " '{print $2}' | awk '{print $1}'`
-sed -i -e "s/listen-address=172.17.0.2/listen-address=$ip/g" /etc/dnsmasq.conf
-sed -i -e "s/listen-address 127.0.0.1:8118/listen-address $ip:8118/g" /etc/privoxy/config
-mv /tmp/privoxy /etc/privoxy/config
-cp /tmp/dnsmasq.conf /etc/dnsmasq.conf
-pihole restartdns
-/etc/init.d/privoxy start
-tail -f /var/log/pihole.log
+dnss -enable_dns_to_https -https_upstream="https://9.9.9.11/dns-query" -enable_dns_to_https
